b5f20ee1635fa4ad94ac67312cc79b0d46e4badc
aakarshan-raj <aakarshan7raj@gmail.com>
2023-06-20 22:46:09 +0530

    starting to write article on mongoose

--- src/pages/nodejs/articles/one/one.js
+++ src/pages/nodejs/articles/one/one.js
@@ -1,5 +1,7 @@
 
 import styles from "./styles.module.css"
+import myImage from "./assets/logo.png"
+
 
 export const ShowOneNodejs = ()=>{
     return (
@@ -9,6 +11,11 @@ export const ShowOneNodejs = ()=>{
         <h1 className={styles.title}>Setting Up Mongoose in Nodejs</h1>
           
             <hr></hr>
+
+            <div className={styles.img_container}>
+             <img src={myImage} className={styles.logo}/>
+            </div>
+            
             <h1>Nothing yet</h1>
          
             
@@ -17,3 +24,4 @@ export const ShowOneNodejs = ()=>{
         </div>
       );
 }
+
