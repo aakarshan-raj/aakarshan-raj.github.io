41937cf2b3b1b730ccd27cdce8e36eed7e27aa44
aakarshan-raj <aakarshan7raj@gmail.com>
2023-06-23 11:52:37 +0530

    added styles

--- src/pages/nodejs/articles/one/one.js
+++ src/pages/nodejs/articles/one/one.js
@@ -1,4 +1,3 @@
-
 import styles from "./styles.module.css"
 import myImage from "./assets/logo.png"
 
@@ -16,8 +15,21 @@ export const ShowOneNodejs = ()=>{
              <img src={myImage} className={styles.logo}/>
             </div>
             
-            <h1>Nothing yet</h1>
-         
+            <div className={styles.content}>
+                 <h1>Mongoose is an popular ODM for MongoDB in Nodejs</h1>
+                 <h2>To get started we will need a databse to play with. Either install MongoDB Locally for get a free account on mongoDB atlas. We will follow the second way
+                  as it</h2>
+            <h2> would be more practical. You first need to create a new cluster then create a new Collection. After than get your "connection string" through </h2>
+            <h2> Connect_to_your_application -&gt; Drivers. Now we have to set up our Nodejs Project.  </h2>
+            
+
+            <div className={styles.code_background}>$ npm install mongoose</div>
+
+
+            </div>
+
+             
+
             
             </div>
           </div>
