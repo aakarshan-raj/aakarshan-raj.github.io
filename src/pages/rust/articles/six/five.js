2fb37a51b11dc6fa90f6f7ebd05b968b5fed6271
aakarshan-raj <aakarshan7raj@gmail.com>
2023-09-08 20:11:57 +0530

    ICED article and separating styles

--- src/pages/rust/articles/six/five.js
+++ src/pages/rust/articles/six/five.js
@@ -1,4 +1,4 @@
-import styles from "./styles.module.css"
+import styles from "../../../../styles/styles.module.css"
 
 
 export const ShowSixRust = () => {
@@ -6,9 +6,57 @@ export const ShowSixRust = () => {
     <div className={styles.book_container}>
       <div className={styles.book}>
         <div className={styles.book_content}>
-          <h1 className={styles.title}>Making GUI application with ICED in Rust</h1>
+          <h1 className={styles.title}>Iced: A Modern Rust GUI Framework for Cross-Platform Applications</h1>
           <hr></hr>
           <div className={styles.content}>
+            <h2>Iced is a modern Rust GUI (Graphical User Interface) framework that simplifies the process of building cross-platform graphical applications.</h2>
+            <h2>in Iced the interface is split into 4 parts</h2>
+            <h2>
+              <h3>1. State(model):state of application</h3>
+              <h3>2. Message: User interaction Or event</h3>
+              <h3>3. View Logic(View): Displaying State to User</h3>
+              <h3>4. Update Logic(update): Reaction to Message and change the state</h3>
+            </h2>
+            <h2>Every Program has these 4 parts.</h2>
+            <h2>There are two ways we can go forward to make GUI in ICED.
+              Every Iced program as a State defined using a struct. This struct can either implement Sandbox trait or Application trait. We will go forward with Sandbox trait first then use Application trait.
+            </h2>
+            <h2>every struct implementing Sandbox trait must implement the following methods</h2>
+            <div className={styles.code_background_pink}>
+              <h2>{code_1}</h2>
+            </div>
+            <h2>We will make a simple GUI that shows "Hello World!"</h2>
+            <div className={styles.code_background}>
+              <h2>{code_2}</h2>
+            </div>
+            <h2>The new function returns the struct type.</h2>
+            <h2>The title returns the title of the window.
+              We haven't done anything in upadate function since there is no user interactions yet.</h2>
+            <h2>The view function will return a simple Text widget then we call .into function to convert it into Element widget.</h2>
+            <h2>Now to run this application we need to call run function on the Struct(model) and provide it with some Settings, at this moment we are using default settings from iced::Settings::default() but later we will provide our own settings.
+              we call unwrap() on it as it returns a Result.</h2>
+
+            <h2>Now we will implement it using Application trait.</h2>
+            <h2>for application trait we need to implement these functions</h2>
+
+            <div className={styles.code_background_pink}>
+              <h2>{code_3}</h2>
+            </div>
+            <h2>and also we need to set these type's</h2>
+            <div className={styles.code_background_red}>
+              <h2>{code_4}</h2>
+            </div>
+            <h2>Executors helpers to execute blocking operations inside async context, we are using the default executer provided by iced.</h2>
+
+            <h2>we have Theme and Flags now, we will use them later, at this moment Theme is set to Theme from iced and flag to a empty Tuple.</h2>
+            <h2>Here is previous code that was written using Sandbox written Using Application trait.</h2>
+            <div className={styles.code_background}>
+              <h2>{code_5}</h2>
+            </div>
+            <h2>the return types have changed for new and update, new returns a tuple now of Self and Command(actions to be performed) at this momment we return Command::none() which means no command.</h2>
+            <h2>update function now returns Command, right now it just returns Command::none() like new.</h2>
+            <h2>if you run this you would get a screen with hello world on it.</h2>
+            <h2>Now we can move to make a somewhat useful Application.</h2>
 
           </div>
         </div>
@@ -16,3 +64,75 @@ export const ShowSixRust = () => {
     </div>
   );
 }
+
+
+const code_1 = `fn new() -> Self { todo!() }
+fn title(&self) -> String { todo!() }
+fn update(&mut self, _: <Self as Sandbox>::Message) { todo!() }
+fn view(&self) -> Element<Message> { todo!() }`;
+const code_2 = `
+
+use iced::widget::Text;
+use iced::{Sandbox, Element};
+
+
+use iced::Settings;
+
+struct Hello;
+
+
+
+impl Sandbox for Hello  {
+type Message = ();
+fn new() -> Self { Self }
+fn title(&self) -> String { String::from("ICED window") }
+fn update(&mut self, _:()) {  }
+fn view(&self) -> Element<()> { Text::new("Hello World!").into() }
+
+}
+
+fn main() {
+   Hello::run(Settings::default()).unwrap()
+}
+`;
+const code_3 = `fn new(_flag:Self::Flags) -> (Self,Command<Self::Message>) { todo!() }
+fn title(&self) -> String { todo!() }
+fn update(&mut self,message:Self::Message) -> Command<Self::Message>{ todo!() }
+fn view(&self) -> Element<Self::Message>{ todo!() }
+ `;
+const code_4 = `type Executor = executor::Default;
+type Message = ();
+type Theme = Theme;
+type Flags = ();`;
+const code_5 = `
+use iced::widget::Text;
+use iced::{Element, Application, Command, executor,Theme};
+
+
+use iced::Settings;
+
+struct Hello;
+
+
+impl Application for Hello  {
+type Executor = executor::Default;
+type Message = ();
+type Theme = Theme;
+type Flags = ();
+
+fn new(_flag:Self::Flags) -> (Self,Command<Self::Message>) {
+      ( 
+        Self,
+        Command::none()
+      )
+     }
+fn title(&self) -> String { String::from("window") }
+fn update(&mut self,_message:Self::Message) -> Command<Self::Message>{ Command::none()}
+fn view(&self) -> Element<Self::Message>{ Text::new("Hello World!").into() }
+ 
+ 
+}
+
+fn main() {
+   Hello::run(Settings::default()).unwrap()
+}`;
