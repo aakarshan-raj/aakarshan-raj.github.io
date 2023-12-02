d90805995ee12b8424e57e804131db6affda1123
aakarshan-raj <aakarshan7raj@gmail.com>
2023-12-03 00:02:17 +0530

    intro

--- src/pages/rust/articles/10/exercises/intro/intro.js
+++ src/pages/rust/articles/10/exercises/intro/intro.js
@@ -10,11 +10,25 @@ export const Intro = () => {
           <h1 className={styles.title}>Intro</h1>
           <hr></hr>
           <div className={styles.sub_heading}>Problem code</div>
+          <FormatCode
+            code={code_1}
+          />
           <div className={styles.sub_heading}>Solution code</div>
+          <FormatCode
+            code={code_2}
+          />
           <div className={styles.sub_heading}>Explaination</div>
+          <h2>There is no function such as printline, to print output buffer to console we have println or print.</h2>
         </div>
       </div>
       <IssueBanner />
     </div>
   );
 }
+
+const code_1 = `fn main() {
+  printline!("Hello there!")
+}`;
+const code_2 = `fn main() {
+  println!("Hello there!");
+}`;
