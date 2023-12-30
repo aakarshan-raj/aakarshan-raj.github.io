411dc881519be65b101e8ca91027962f709cff46
aakarshan-raj <aakarshan7raj@gmail.com>
2023-12-30 18:33:41 +0530

    options

--- src/pages/rust/articles/10/exercises/hashmaps/hashmap.js
+++ src/pages/rust/articles/10/exercises/hashmaps/hashmap.js
@@ -1,17 +1,426 @@
 import IssueBanner from "../../../../../../main_components/issue_banner";
 import styles from "../../../../../../styles/styles.module.css"
 import FormatCode from "../../../../../../main_components/code_formatter"
+import SubHeading from "../../../../../../main_components/sub_heading"
 
 export const Hashmaps = () => {
-  return (
-    <div className={styles.book_container}>
-      <div className={styles.book}>
-        <div className={styles.book_content}>
-          <h1 className={styles.title}>Hashmaps</h1>
-          <hr></hr>
+    return (
+        <div className={styles.book_container}>
+            <div className={styles.book}>
+                <div className={styles.book_content}>
+                    <h1 className={styles.title}>Hashmaps</h1>
+                    <hr></hr>
+
+                    <SubHeading text="Problem code: Hashmaps1.rs" />
+                    <FormatCode code={code_1} />
+                    <SubHeading text="Solution code" />
+                    <FormatCode code={code_2} />
+                    <SubHeading text="Explanation" />
+                    <h2>Declaring a hashmap that takes key as string and value as u32 then insert key-value pair values to pass the tests</h2>
+                    <hr>
+                    </hr>
+
+                    <SubHeading text="Problem code: Hashmaps2.rs" />
+                    <FormatCode code={code_3} />
+                    <SubHeading text="Solution code" />
+                    <FormatCode code={code_4} />
+                    <SubHeading text="Explanation" />
+                    <h2>`contains_key()` contains key can be used to check if the key exists in the Hasmap.
+                    </h2>
+                    <hr>
+                    </hr>
+
+                    <SubHeading text="Problem code: Hashmaps3.rs" />
+                    <FormatCode code={code_5} />
+                    <SubHeading text="Solution code" />
+                    <FormatCode code={code_6} />
+                    <SubHeading text="Explanation" />
+                    <h2>written logic to check if a certain key exists if it does they get the value for that key and add the current goals, the logic can be written with less code that this.
+                    </h2>
+                    <hr>
+                    </hr>
+
+
+                </div>
+            </div>
+            <IssueBanner />
         </div>
-      </div>
-      <IssueBanner />
-    </div>
-  );
+    );
 }
+const code_1 = `use std::collections::HashMap;
+
+fn fruit_basket() -> HashMap<String, u32> {
+    let mut basket = // TODO: declare your hash map here.
+
+    // Two bananas are already given for you :)
+    basket.insert(String::from("banana"), 2);
+
+    // TODO: Put more fruits in your basket here.
+
+    basket
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn at_least_three_types_of_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.len() >= 3);
+    }
+
+    #[test]
+    fn at_least_five_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.values().sum::<u32>() >= 5);
+    }
+}
+`;
+const code_2 = `use std::collections::HashMap;
+
+fn fruit_basket() -> HashMap<String, u32> {
+    let mut basket = HashMap::new();
+    // Two bananas are already given for you :)
+    basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("sav"), 1);
+    basket.insert(String::from("santra"), 1);
+    basket.insert(String::from("naspati"), 1);
+    basket
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn at_least_three_types_of_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.len() >= 3);
+    }
+
+    #[test]
+    fn at_least_five_fruits() {
+        let basket = fruit_basket();
+        assert!(basket.values().sum::<u32>() >= 5);
+    }
+}
+`;
+const code_3 = `use std::collections::HashMap;
+
+#[derive(Hash, PartialEq, Eq)]
+enum Fruit {
+    Apple,
+    Banana,
+    Mango,
+    Lychee,
+    Pineapple,
+}
+
+fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
+    let fruit_kinds = vec![
+        Fruit::Apple,
+        Fruit::Banana,
+        Fruit::Mango,
+        Fruit::Lychee,
+        Fruit::Pineapple,
+    ];
+
+    for fruit in fruit_kinds {
+        // TODO: Insert new fruits if they are not already present in the
+        // basket. Note that you are not allowed to put any type of fruit that's
+        // already present!
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Don't modify this function!
+    fn get_fruit_basket() -> HashMap<Fruit, u32> {
+        let mut basket = HashMap::<Fruit, u32>::new();
+        basket.insert(Fruit::Apple, 4);
+        basket.insert(Fruit::Mango, 2);
+        basket.insert(Fruit::Lychee, 5);
+
+        basket
+    }
+
+    #[test]
+    fn test_given_fruits_are_not_modified() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
+        assert_eq!(*basket.get(&Fruit::Mango).unwrap(), 2);
+        assert_eq!(*basket.get(&Fruit::Lychee).unwrap(), 5);
+    }
+
+    #[test]
+    fn at_least_five_types_of_fruits() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        let count_fruit_kinds = basket.len();
+        assert!(count_fruit_kinds >= 5);
+    }
+
+    #[test]
+    fn greater_than_eleven_fruits() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        let count = basket.values().sum::<u32>();
+        assert!(count > 11);
+    }
+    
+    #[test]
+    fn all_fruit_types_in_basket() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        for amount in basket.values() {
+            assert_ne!(amount, &0);
+        }
+    }
+}
+`;
+const code_4 = `
+use std::collections::HashMap;
+
+#[derive(Hash, PartialEq, Eq)]
+enum Fruit {
+    Apple,
+    Banana,
+    Mango,
+    Lychee,
+    Pineapple,
+}
+
+fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
+    let fruit_kinds = vec![
+        Fruit::Apple,
+        Fruit::Banana,
+        Fruit::Mango,
+        Fruit::Lychee,
+        Fruit::Pineapple,
+    ];
+
+    for fruit in fruit_kinds {
+        if(basket.contains_key(&fruit)){
+
+        }
+        else{
+            basket.insert(fruit,23);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Don't modify this function!
+    fn get_fruit_basket() -> HashMap<Fruit, u32> {
+        let mut basket = HashMap::<Fruit, u32>::new();
+        basket.insert(Fruit::Apple, 4);
+        basket.insert(Fruit::Mango, 2);
+        basket.insert(Fruit::Lychee, 5);
+
+        basket
+    }
+
+    #[test]
+    fn test_given_fruits_are_not_modified() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
+        assert_eq!(*basket.get(&Fruit::Mango).unwrap(), 2);
+        assert_eq!(*basket.get(&Fruit::Lychee).unwrap(), 5);
+    }
+
+    #[test]
+    fn at_least_five_types_of_fruits() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        let count_fruit_kinds = basket.len();
+        assert!(count_fruit_kinds >= 5);
+    }
+
+    #[test]
+    fn greater_than_eleven_fruits() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        let count = basket.values().sum::<u32>();
+        assert!(count > 11);
+    }
+    
+    #[test]
+    fn all_fruit_types_in_basket() {
+        let mut basket = get_fruit_basket();
+        fruit_basket(&mut basket);
+        for amount in basket.values() {
+            assert_ne!(amount, &0);
+        }
+    }
+}`;
+const code_5 = `
+use std::collections::HashMap;
+
+// A structure to store the goal details of a team.
+struct Team {
+    goals_scored: u8,
+    goals_conceded: u8,
+}
+
+fn build_scores_table(results: String) -> HashMap<String, Team> {
+    // The name of the team is the key and its associated struct is the value.
+    let mut scores: HashMap<String, Team> = HashMap::new();
+
+    for r in results.lines() {
+        let v: Vec<&str> = r.split(',').collect();
+        let team_1_name = v[0].to_string();
+        let team_1_score: u8 = v[2].parse().unwrap();
+        let team_2_name = v[1].to_string();
+        let team_2_score: u8 = v[3].parse().unwrap();
+        // TODO: Populate the scores table with details extracted from the
+        // current line. Keep in mind that goals scored by team_1
+        // will be the number of goals conceded from team_2, and similarly
+        // goals scored by team_2 will be the number of goals conceded by
+        // team_1.
+    }
+    scores
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_results() -> String {
+        let results = "".to_string()
+            + "England,France,4,2\n"
+            + "France,Italy,3,1\n"
+            + "Poland,Spain,2,0\n"
+            + "Germany,England,2,1\n";
+        results
+    }
+
+    #[test]
+    fn build_scores() {
+        let scores = build_scores_table(get_results());
+
+        let mut keys: Vec<&String> = scores.keys().collect();
+        keys.sort();
+        assert_eq!(
+            keys,
+            vec!["England", "France", "Germany", "Italy", "Poland", "Spain"]
+        );
+    }
+
+    #[test]
+    fn validate_team_score_1() {
+        let scores = build_scores_table(get_results());
+        let team = scores.get("England").unwrap();
+        assert_eq!(team.goals_scored, 5);
+        assert_eq!(team.goals_conceded, 4);
+    }
+
+    #[test]
+    fn validate_team_score_2() {
+        let scores = build_scores_table(get_results());
+        let team = scores.get("Spain").unwrap();
+        assert_eq!(team.goals_scored, 0);
+        assert_eq!(team.goals_conceded, 2);
+    }
+}`;
+const code_6 = `
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Team {
+    goals_scored: u8,
+    goals_conceded: u8,
+}
+
+fn build_scores_table(results: String) -> HashMap<String, Team> {
+    // The name of the team is the key and its associated struct is the value.
+    let mut scores: HashMap<String, Team> = HashMap::new();
+
+    for r in results.lines() {
+        let v: Vec<&str> = r.split(',').collect();
+        let team_1_name = v[0].to_string();
+        let team_1_score: u8 = v[2].parse().unwrap();
+        let team_2_name = v[1].to_string();
+        let team_2_score: u8 = v[3].parse().unwrap();
+        // check if it exists
+        if (scores.get(&team_1_name).is_some()) {
+            let x = scores.get(&team_1_name).unwrap();
+            println!("MEOW:\n{:?}", x);
+            let team1 = Team {
+                goals_scored: team_1_score + x.goals_scored,
+                goals_conceded: x.goals_conceded + team_2_score,
+            };
+            scores.insert(team_1_name, team1);
+        } else {
+            let team1 = Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            };
+            scores.insert(team_1_name, team1);
+        }
+        if (scores.get(&team_2_name).is_some()) {
+            let x = scores.get(&team_2_name).unwrap();
+            let team2 = Team {
+                goals_scored: team_2_score + x.goals_scored,
+                goals_conceded: team_1_score + x.goals_conceded,
+            };
+            scores.insert(team_2_name, team2);
+        } else {
+            let team2 = Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            };
+            scores.insert(team_2_name, team2);
+        }
+    }
+    scores
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_results() -> String {
+        let results = "".to_string()
+            + "England,France,4,2\n"
+            + "France,Italy,3,1\n"
+            + "Poland,Spain,2,0\n"
+            + "Germany,England,2,1\n";
+        results
+    }
+
+    #[test]
+    fn build_scores() {
+        let scores = build_scores_table(get_results());
+
+        let mut keys: Vec<&String> = scores.keys().collect();
+        keys.sort();
+        assert_eq!(
+            keys,
+            vec!["England", "France", "Germany", "Italy", "Poland", "Spain"]
+        );
+    }
+
+    #[test]
+    fn validate_team_score_1() {
+        let scores = build_scores_table(get_results());
+        let team = scores.get("England").unwrap();
+        assert_eq!(team.goals_scored, 5);
+        assert_eq!(team.goals_conceded, 4);
+    }
+
+    #[test]
+    fn validate_team_score_2() {
+        let scores = build_scores_table(get_results());
+        let team = scores.get("Spain").unwrap();
+        assert_eq!(team.goals_scored, 0);
+        assert_eq!(team.goals_conceded, 2);
+    }
+}`;
--- src/pages/rust/articles/10/exercises/options/options.js
+++ src/pages/rust/articles/10/exercises/options/options.js
@@ -1,6 +1,8 @@
 import IssueBanner from "../../../../../../main_components/issue_banner";
 import styles from "../../../../../../styles/styles.module.css"
 import FormatCode from "../../../../../../main_components/code_formatter"
+import SubHeading from "../../../../../../main_components/sub_heading"
+
 export const Options = () => {
   return (
     <div className={styles.book_container}>
@@ -8,9 +10,201 @@ export const Options = () => {
         <div className={styles.book_content}>
           <h1 className={styles.title}>Options</h1>
           <hr></hr>
+
+          <SubHeading text="Problem code: options1.rs" />
+          <FormatCode code={code_1} />
+          <SubHeading text="Solution code" />
+          <FormatCode code={code_2} />
+          <SubHeading text="Explanation" />
+          <h2>Written logic to return Option variant Some with 5 value if the time_of_day is less than 22, if more than 24 return None, also to test values to option wrap the data in Some.</h2>
+          <hr>
+          </hr>
+
+          <SubHeading text="Problem code: options2.rs" />
+          <FormatCode code={code_3} />
+          <SubHeading text="Solution code" />
+          <FormatCode code={code_4} />
+          <SubHeading text="Explanation" />
+          <h2> used if let which is speically used Options, when we use if let we extract the value from the option and can use that value. also .pop returns a Option that's why we had to wrap cursor in Some.</h2>
+          <hr>
+          </hr>
+
+          <SubHeading text="Problem code: options3.rs" />
+          <FormatCode code={code_5} />
+          <SubHeading text="Solution code" />
+          <FormatCode code={code_6} />
+          <SubHeading text="Explanation" />
+          <h2>The match block took ownership of our Struct, using ref keyword solves it as.</h2>
+          <h2>Why not use &, what is ref?
+ref have no role in pattern matching, if we use & then it means their would be a reference in pattern matching, but we dont want that, we wanna pass a value, use it in match block without giving it the ownership.
+ref would indicate we want a reference to the value that matched.
+</h2>
+          <hr>
+          </hr>
+
         </div>
       </div>
       <IssueBanner />
     </div>
   );
 }
+const code_1 = `
+fn maybe_icecream(time_of_day: u16) -> Option<u16> {
+    // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a
+    // value of 0 The Option output should gracefully handle cases where
+    // time_of_day > 23.
+    // TODO: Complete the function body - remember to return an Option!
+    ???
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_icecream() {
+        assert_eq!(maybe_icecream(9), Some(5));
+        assert_eq!(maybe_icecream(10), Some(5));
+        assert_eq!(maybe_icecream(23), Some(0));
+        assert_eq!(maybe_icecream(22), Some(0));
+        assert_eq!(maybe_icecream(25), None);
+    }
+
+    #[test]
+    fn raw_value() {
+        // TODO: Fix this test. How do you get at the value contained in the
+        // Option?
+        let icecreams = maybe_icecream(12);
+        assert_eq!(icecreams, 5);
+    }
+}`;
+const code_2 = `fn maybe_icecream(time_of_day: u16) -> Option<u16> {
+
+  let mut ice_cream: Option<u16>;
+  if time_of_day < 22 {
+      ice_cream = Some(5);
+  } else if (time_of_day < 25){
+      ice_cream = Some(0);
+  }
+  else{
+      ice_cream = None;
+  }
+  ice_cream
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn check_icecream() {
+      assert_eq!(maybe_icecream(9), Some(5));
+      assert_eq!(maybe_icecream(10), Some(5));
+      assert_eq!(maybe_icecream(23), Some(0));
+      assert_eq!(maybe_icecream(22), Some(0));
+      assert_eq!(maybe_icecream(25), None);
+  }
+
+  #[test]
+  fn raw_value() {
+      let icecreams = maybe_icecream(12);
+      assert_eq!(icecreams, Option::Some(5));
+  }
+}
+`;
+const code_3 = `#[cfg(test)]
+mod tests {
+    #[test]
+    fn simple_option() {
+        let target = "rustlings";
+        let optional_target = Some(target);
+
+        // TODO: Make this an if let statement whose value is "Some" type
+        word = optional_target {
+            assert_eq!(word, target);
+        }
+    }
+
+    #[test]
+    fn layered_option() {
+        let range = 10;
+        let mut optional_integers: Vec<Option<i8>> = vec![None];
+
+        for i in 1..(range + 1) {
+            optional_integers.push(Some(i));
+        }
+
+        let mut cursor = range;
+        integer = optional_integers.pop() {
+            assert_eq!(integer, cursor);
+            cursor -= 1;
+        }
+
+        assert_eq!(cursor, 0);
+    }
+}
+`;
+const code_4 = `#[cfg(test)]
+mod tests {
+    #[test]
+    fn simple_option() {
+        let target = "rustlings";
+        let optional_target = Some(target);
+
+        // TODO: Make this an if let statement whose value is "Some" type
+        if let Some(word) = optional_target {
+            assert_eq!(word, target);
+        }
+    }
+
+    #[test]
+    fn layered_option() {
+        let range = 10;
+        let mut optional_integers: Vec<Option<i8>> = vec![None];
+
+        for i in 1..(range + 1) {
+            optional_integers.push(Some(i));
+        }
+
+        let mut cursor = range;
+
+        while(cursor != 0){
+        if let Some(integer) = optional_integers.pop() {
+            assert_eq!(integer, Some(cursor));
+            cursor -= 1;
+        }
+    }
+
+        assert_eq!(cursor, 0);
+    }
+}`;
+const code_5 = `struct Point {
+  x: i32,
+  y: i32,
+}
+
+fn main() {
+  let y: Option<Point> = Some(Point { x: 100, y: 200 });
+
+  match y {
+      Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+      _ => panic!("no match!"),
+  }
+  y; // Fix without deleting this line.
+}
+`;
+const code_6 = `struct Point {
+  x: i32,
+  y: i32,
+}
+
+fn main() {
+  let y: Option<Point> = Some(Point { x: 100, y: 200 });
+
+  match y {
+      Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+      _ => panic!("no match!"),
+  }
+  y; // Fix without deleting this line.
+}
+`;
