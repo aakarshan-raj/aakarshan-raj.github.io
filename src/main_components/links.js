671ebf971cd255ed506fa301bab8d623809ffe78
aakarshan-raj <aakarshan7raj@gmail.com>
2024-03-10 23:30:32 +0530

    change in lists

--- src/main_components/links.js
+++ src/main_components/links.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Drawer from '@mui/material/Drawer';
+import styles from "../styles/styles.module.css"
 
 export const ShowLinks = (props) => {
   return (
@@ -9,24 +10,24 @@ export const ShowLinks = (props) => {
           <Drawer anchor="left" open={props.state} onClose={props.stateFunction}>
             <div className="drawer">
               <ol className="drawer-list">
-                <li><Link to={"/nodejs"}  onClick={props.stateFunction} className="drawer-item">NodeJS </Link></li>
-                <li><Link to={"/react"}  onClick={props.stateFunction} className="navbar-item">REACT</Link></li>
-                <li><Link to={"/alu"}  onClick={props.stateFunction} className="drawer-item">ALU </Link></li>
-                <li><Link to={"/ram16k"}  onClick={props.stateFunction}  className="drawer-item">RAM16K </Link></li>
-                <li><Link to={"/cpu"}  onClick={props.stateFunction} className="drawer-item">CPU </Link></li>
-                <li><Link to={"/computer"}  onClick={props.stateFunction}  className="drawer-item">COMPUTER </Link></li>
-                <li><Link to={"/graphs"}  onClick={props.stateFunction}  className="drawer-item">Graphs </Link></li>
+                <li><Link to={"/nodejs"}  onClick={props.stateFunction} className={`${styles.nav_link} navbar-item`}>+NodeJS </Link></li>
+                <li><Link to={"/react"}  onClick={props.stateFunction} className={`${styles.nav_link} navbar-item`}>+REACT</Link></li>
+                <li><Link to={"/alu"}  onClick={props.stateFunction} className={`${styles.nav_link} navbar-item`}>+ALU </Link></li>
+                <li><Link to={"/ram16k"}  onClick={props.stateFunction} className={`${styles.nav_link} navbar-item`}>+RAM16K </Link></li>
+                <li><Link to={"/cpu"}  onClick={props.stateFunction} className={`${styles.nav_link} navbar-item`}>+CPU </Link></li>
+                <li><Link to={"/computer"}  onClick={props.stateFunction}  className={`${styles.nav_link} navbar-item`}>+COMPUTER </Link></li>
+                <li><Link to={"/graphs"}  onClick={props.stateFunction} className={`${styles.nav_link} navbar-item`}>+Graphs </Link></li>
 
               </ol>
             </div>
 
           </Drawer>
 
-          <Link to={"/rust"} className="navbar-item">RUST</Link>
-          <Link to={"/assembly"} className="navbar-item">ASSEMBLY</Link>
-          <Link to={"/cpp"} className="navbar-item">C++</Link>
-          <Link to={"/books"} className="navbar-item">BOOKS</Link>
-          <Link className="navbar-item" onClick={props.stateFunction}>MORE</Link>
+          <Link to={"/rust"} className={`${styles.nav_link} navbar-item`}>RUST</Link>
+          <Link to={"/assembly"} className={`${styles.nav_link} navbar-item`}>ASSEMBLY</Link>
+          <Link to={"/cpp"} className={`${styles.nav_link} navbar-item`}>C++</Link>
+          <Link to={"/books"} className={`${styles.nav_link} navbar-item`}>BOOKS</Link>
+          <Link className={`${styles.nav_link} navbar-item`}onClick={props.stateFunction}>MORE</Link>
 
         </ul>
       </nav>
