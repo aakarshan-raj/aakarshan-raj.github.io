aecd07f4b8cc0259832a2e1edd9171f421bd6ddf
aakarshan-raj <aakarshan7raj@gmail.com>
2024-05-27 17:35:49 +0530

    process API

--- src/pages/more/Os/Virtualization/virtualization.js
+++ src/pages/more/Os/Virtualization/virtualization.js
@@ -13,7 +13,7 @@ export const ShowVirtualization = (props) => {
       <hr></hr>
       <div className={styles.book_content}>
         <SubHeading
-          text={"The Abstraction: The Process"}
+          text={"The Process"}
           size={""}
           start_color={"#ff0f3f"}
           end_color={"#51ed4c"}
@@ -24,6 +24,17 @@ export const ShowVirtualization = (props) => {
           <li>  If we have a lot of processes, don't wait for a process to end to give control to a process that needs to request I/O. We can use the
             time when it goes into a blocking state due to user input as an opportunity to run our process. If we give it control at the end, it will take a lot of time.</li>
           <li>   Switch processes without waiting for the process to fully end when I/O instructions constitute more than ~50% of its workload.</li>
+        </ol></h2>
+        <hr />
+        <SubHeading
+          text={"The Process API"}
+          size={""}
+          start_color={"#ff0f3f"}
+          end_color={"#51ed4c"}
+        />
+        <h2> <ol>
+          <li></li>
+          
         </ol></h2>
       </div>
 
