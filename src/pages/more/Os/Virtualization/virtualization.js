8c960b17f43bf0ee568faed62e7dcfa647424088
aakarshan-raj <aakarshan7raj@gmail.com>
2024-05-28 16:51:33 +0530

    process api done

--- src/pages/more/Os/Virtualization/virtualization.js
+++ src/pages/more/Os/Virtualization/virtualization.js
@@ -1,6 +1,7 @@
 import styles from '../../../../styles/styles.module.css';
 import { useEffect } from 'react';
 import SubHeading from '../../../../main_components/sub_heading'
+import FormatCode from '../../../../main_components/code_formatter';
 
 export const ShowVirtualization = (props) => {
   const title = "Virtualization";
@@ -33,8 +34,29 @@ export const ShowVirtualization = (props) => {
           end_color={"#51ed4c"}
         />
         <h2> <ol>
-          <li></li>
-          
+          <li>fork starts another proceess and executes the code after fork call in original program in that process.</li>
+          <li>fork() returns 0 if it succdeed and a negative number if it failed.</li>
+          <li>The output becomes non-determinstic since the process execution is handled by CPU schduler.</li>
+          <li>wait() system call makes parent process to wait for child process to complete its execution.</li>
+          <li>wait(NULL) on linux would return pid of child process.</li>
+          <li>exec() family of system callls replace the runnning process image with a new process image, overwritting code segment, re-initialising heap and stack memory.</li>
+          <li>execvp a system call of exec family takes argument of program name and arguments with null terminating.</li>
+          <FormatCode
+            code={code_1}
+            lang="c"
+          />
+          <li>example: unix shell</li>
+          <ol>
+            <li>Write a argument in shell</li>
+            <li>shell calls fork</li>
+            <li>inside child prcess exec is called with argument </li>
+            <li>in parent process we call wait() so we wait for child process to complete</li>
+            <li>repeat</li>
+          </ol>
+          <li>A/C Unix security model only parent process can terminate child process. the first process is known as init</li>
+          <li>An orphan process is adopted by init process(pid:1)</li>
+
+
         </ol></h2>
       </div>
 
@@ -42,3 +64,10 @@ export const ShowVirtualization = (props) => {
   );
 }
 
+const code_1 = `       else if(rc == 0){
+          printf("Child process ID:%d",(int)getpid());
+          char *args[2];
+          args[0] = "ls";
+          args[1] = NULL;
+          execvp(args[0],args);
+       }`
