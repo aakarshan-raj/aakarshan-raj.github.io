60fa85b1ad72a4bafb894357e40b8d5d46ea1827
aakarshan-raj <aakarshan7raj@gmail.com>
2025-01-02 20:41:55 +0530

    update article

--- src/pages/articles/article1.js
+++ src/pages/articles/article1.js
@@ -17,11 +17,11 @@ export const Article1 = (props) => {
                 <ol>
                     <li>Amdahl’s law states that the overall performance improvement gained by
                         optimizing a single part of a system is limited by the fraction of time that the
-                        improved part is actually used</li>
+                        improved part is actually used</li><br></br>
                     <li>Gustafson’s law tells us that as long as we find ways to keep our extra
                         resources busy, the speedup should continue to increase and not be limited by
-                        the serial part of the problem.</li>
-                    <li>Execution cycle of a command</li>
+                        the serial part of the problem.</li><br></br>
+                    <li>Execution cycle of a command</li><br></br>
                     <ol>
                         <li>Job goes in a job queue</li>
                         <li>Once ready moved to ready queue</li>
@@ -32,24 +32,25 @@ export const Article1 = (props) => {
                         <li>CPU picks the job and executes the instructions</li>
                         <li> If a interrupt is raise CPU picks another job</li>
                         <li>original job is again put in ready queue and will be picked by CPU</li>
-                    </ol>
-                    <li>information about a job is stored in PCB(process context block)</li>
+                    </ol><br></br>
+                    <li>information about a job is stored in PCB(process context block)</li><br></br>
                     <li> goroutines are user level threads, which are mapped to OS threads, M go routines are multiplexed to N OS threads, Go scheduler can move these goroutine from one thread to another and this all happens in one processs</li>
-
-                    <li>atomic operation: instructions that cannot be interuppted</li>
+                    <br></br>
+                    <li>atomic operation: instructions that cannot be interuppted</li><br></br>
                     <li>A race condition occurs when two or more threads (go routines) try to access the same critical section concurrently, and at least one of them is trying to modify shared data.</li>
+                    <br></br>
                     <li>mutex locks other threads from executing code that is locked by another thread, only after the first thread is done the suspending mutex can access the code</li>
-                    <li>A reader-preferred lock ensures that all read locks are released before a write lock can be acquired. This design allows multiple readers to access the resource simultaneously but requires that no readers are active before a writer can proceed.</li>
-                    <li>condition variables come in. Condition variables work togetherwith mutexes and give us the ability to block the current execution until we have a signal that a particular condition has changed.</li>
-                    <li> using cond we can make one go routine wait for a condition to be true and start its execution</li>
-                    <li> Calling Wait() Unlocks the mutex and suspends a go routine and using Signal() from another goroutine starts it again, This is called monitor pattern using mutex and conditional variables, it can be used to order the execution of the program. Ex: 5.7 (Calling signal between Mutex Lock and Unlock in a go routine will only be executed if Wait releases the lock)</li>
-                    <li>To execute wait and signal we need to call Lock() before.</li>
-                    <li> Broadcast unlike signal which only resumes one of the suspended go routine, broadcast resumes all suspended go routines, after suspended go routine resume it starts its execution after the line where Wait() was called.</li>
-                    <li>
+                    <br></br><li>A reader-preferred lock ensures that all read locks are released before a write lock can be acquired. This design allows multiple readers to access the resource simultaneously but requires that no readers are active before a writer can proceed.</li>
+                    <br></br>      <li>condition variables come in. Condition variables work togetherwith mutexes and give us the ability to block the current execution until we have a signal that a particular condition has changed.</li>
+                    <br></br>      <li> using cond we can make one go routine wait for a condition to be true and start its execution</li>
+                    <br></br>      <li> Calling Wait() Unlocks the mutex and suspends a go routine and using Signal() from another goroutine starts it again, This is called monitor pattern using mutex and conditional variables, it can be used to order the execution of the program. Ex: 5.7 (Calling signal between Mutex Lock and Unlock in a go routine will only be executed if Wait releases the lock)</li>
+                    <br></br>      <li>To execute wait and signal we need to call Lock() before.</li>
+                    <br></br>      <li> Broadcast unlike signal which only resumes one of the suspended go routine, broadcast resumes all suspended go routines, after suspended go routine resume it starts its execution after the line where Wait() was called.</li>
+                    <br></br>       <li>
                         write preferred lock gives writer part of mutex control first before reader gets it.
                     </li>
-                    Semaphore are like mutex but it can allow N number of go routines access to a resource and can be implemented with a counter and Cond
-                    <li></li>
+                    <br></br>
+                    <li>  Semaphore are like mutex but it can allow N number of go routines access to a resource and can be implemented with a counter and Cond</li>
                 </ol>
             </h2>
 
--- src/pages/articles/article3.js
+++ src/pages/articles/article3.js
@@ -13,7 +13,35 @@ export const Article3 = (props) => {
             <h1 className={styles.title}></h1>
             <hr></hr>
             <h2>
-              
+                <ol>
+                    <li>Before C++11, there was no standard C++ library that supported multithreading. Developers had to rely on operating system-specific APIs (such as POSIX Threads) or third-party libraries to implement multithreading.
+                    </li>
+                  <br></br>
+                    <li>Hardware threads is measure of how many independent tasks the hardware can genuinely run concurrently.
+                    </li>
+                    <br></br>
+            
+                    <li>Concurrency can be approached in two main ways: Multiple Processes, Single Thread and One Process, Multiple Threads. In the former, multiple processes are used, each running a single thread that executes tasks independently. In the latter, a single process contains multiple threads, each handling tasks concurrently within the same process.</li>
+                    <br></br>
+            
+                    <li>C++ implements concurrency primarily through multithreading, utilizing multiple threads within a single process. since it's much easier in terms of less bookkeeping, faster data sharing.
+            
+                    </li>
+                    <br></br>
+            
+                    <li>C++ does not provide a standard library for IPC, similar to Go, both languages rely on OS-level mechanisms or third-party libraries for IPC. some languages, such as Erlang, use a concurrency model built around processes.
+                    </li>
+                    <br></br>
+            
+                    <li>Task parallelism: dividing one tasks into multiple and run them in parallel</li>
+                    <br></br>
+            
+                    <li>Data parallelism: Multiple threads performing same task on different parts of data</li>
+                    <br></br>
+            
+                    <li>There is no benefit of using concurrency if the task cannot be parallelized, or if performance gain from using concurrency is not significant emough.</li>
+
+                </ol>
             </h2>
 
         </>
