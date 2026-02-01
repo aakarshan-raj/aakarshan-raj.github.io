3568a292d7122c69b47da24c9482c2111bead4ab
aakarshan-raj <aakarshan7raj@gmail.com>
2026-02-01 19:59:17 +0530

    update landing page

--- src/pages/main/ShowMain.js
+++ src/pages/main/ShowMain.js
@@ -1,7 +1,7 @@
 import styles from '../../styles/styles.module.css';
 import IssueBanner from '../../main_components/issue_banner'
 import SubHeading from '../../main_components/sub_heading'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
 
@@ -10,72 +10,14 @@ export const ShowMain = (props) => {
   useEffect(() => {
     props.setTitle(title)
   }, []);
+  const expandHideArticleList = () => {
+    setArticleListExpanded(!articleListExpanded)
+  }
+
+  const [articleListExpanded, setArticleListExpanded] = useState(false)
+
   return (
     <>
-      <div className={styles.title}>
-        <h6>
-          
-            <Link to="/interpreter/parser">How do parsers work?</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/Assembly/instruction-set">Instruction Set</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/NandToTetris/cpu">Implementing a CPU</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/NandToTetris/ram16k">RAM from scrach</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/NandToTetris/computer">Assembling computer with previous components</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/Assembly/winDbg-intel-manual">Getting started with winDbg and intel manual</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/rust/four">Chat Server in Rust(tokio)</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/rust/six">Making GUI application with ICED in Rust</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/cpp/ecs">ECS Design Pattern</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/cpp/collision-detection-and-resolution">Collision Detection in Games</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/articles/concurrency-in-action">Lessons learnt from "C++ Concurrency in Action"</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/graph/one">Path between two vertices</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/graph/two">Number of connected components in a Graph</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/graph/compute-scc-dg">Compute SCC of a Directed Graph</Link>
-          <br></br>
-          <br></br>
-          
-            <Link to="/database/b+tree-and-dbms">B+Tree and its use in DBMS</Link>
-          <br></br>
-          <br></br>
-        </h6>
-      </div>
       <h2 className={styles.title}>
         <SubHeading text="RSA key for any communication" />
       </h2>
@@ -83,6 +25,74 @@ export const ShowMain = (props) => {
         <div className={styles.code_background_rsa}>
           <h2>{keys}</h2>
         </div>
+      </div>
+      <div className={styles.title}>
+        <h6>
+          <div className={styles.expand_link} onClick={expandHideArticleList}>
+            Articles {articleListExpanded ? '↑' : '↓'}
+          </div>
+          {articleListExpanded && <div>
+            <Link to="/interpreter/parser">How do parsers work?</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/Assembly/instruction-set">Instruction Set</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/NandToTetris/cpu">Implementing a CPU</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/NandToTetris/ram16k">RAM from scrach</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/NandToTetris/computer">Assembling computer with previous components</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/Assembly/winDbg-intel-manual">Getting started with winDbg and intel manual</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/rust/four">Chat Server in Rust(tokio)</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/rust/six">Making GUI application with ICED in Rust</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/cpp/ecs">ECS Design Pattern</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/cpp/collision-detection-and-resolution">Collision Detection in Games</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/articles/concurrency-in-action">Lessons learnt from "C++ Concurrency in Action"</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/graph/one">Path between two vertices</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/graph/two">Number of connected components in a Graph</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/graph/compute-scc-dg">Compute SCC of a Directed Graph</Link>
+            <br></br>
+            <br></br>
+
+            <Link to="/database/b+tree-and-dbms">B+Tree and its use in DBMS</Link>
+            <br></br>
+            <br></br>
+          </div>}
+        </h6>
       </div>
     </>
 
